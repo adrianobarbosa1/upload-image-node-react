0b25cdf8d2148ee8579c2756ddebf8631cc746bf
Adriano Barbosa <adriano.barbosa100@gmail.com>
2022-10-22 10:55:03 -0300

    add errorHandler

---
+++ server/src/errors/databaseConnectionError.ts
@@ -0,0 +1,15 @@
+import { CustomError } from "./customError"
+
+export class DatabaseConnectError extends CustomError {
+  statusCode = 500
+  reason = "Erro ao conectar com o MongoDB"
+  constructor() {
+    super("Erro ao conectar com o MongoDB")
+
+    Object.setPrototypeOf(this, DatabaseConnectError.prototype)
+  }
+
+  serializeErrors() {
+    return [{ message: this.reason }]
+  }
+}
